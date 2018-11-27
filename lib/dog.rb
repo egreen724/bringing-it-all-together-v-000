0d74d65508eb0cbbbf49f542ac546033221eea91
Erin Green <erin.y.green@gmail.com>
2018-11-27 01:51:14 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -33,7 +33,7 @@ class Dog
     id = row[0]
     name = row[1]
     breed = row[2]
-    new_dog = Dog.new(id, name, breed)
+    new_dog = Dog.new(id: id, name: name, breed: breed)
   end
   
   def find_by_name 
