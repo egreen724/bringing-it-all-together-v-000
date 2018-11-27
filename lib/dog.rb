36d12dc38affa41083502ada50b576ddba05e6aa
Erin Green <erin.y.green@gmail.com>
2018-11-27 02:00:55 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -45,7 +45,8 @@ class Dog
   
   def self.create(name, breed)
     new_dog = Dog.new(name: name, breed: breed)
-    new_dog.save 
+    new_dog.save
+    new_dog
   end
   
   def self.new_from_db(row)
