2340a391482e401770419270e908cbc0ad7dd526
Erin Green <erin.y.green@gmail.com>
2018-11-27 02:13:13 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -79,7 +79,17 @@ class Dog
     end.first
   end
   
-  def self.find_or_create_by 
+  def self.find_or_create_by(name: name, breed: breed)
+     dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
+    if !song.empty?
+      song_data = song[0]
+      song = Song.new(song_data[0], song_data[1], song_data[2])
+    else
+      song = self.create(name: name, album: album)
+    end
+    song
+  end 
+end 
   
   def update 
     sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
